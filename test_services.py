39bfa04ab5df5838618be26d62a149203582ac6b
estudart <erico.studartdf@gmail.com>
2025-08-01 10:50:48 -0300

    feat: add test deallocation

--- test_services.py
+++ test_services.py
@@ -25,6 +25,7 @@ class FakeRepository:
     def list(self):
         return list(self._batches)
 
+
 def test_returns_allocation():
     line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
     batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
@@ -32,6 +33,15 @@ def test_returns_allocation():
 
     result = services.allocate(line, repo, FakeSession())  #(2) (3)
     assert result == "b1"
+
+def test_can_deallocate():
+    line = model.OrderLine("o4", "TINY-DESK", 10)
+    batch = model.Batch("ref1", "TINY-DESK", 100, eta=None)
+    repo = FakeRepository([batch])
+
+    services.allocate(line, repo, FakeSession())
+    result = services.deallocate(line, repo, FakeSession())
+    assert result == "ref1"
 
 def test_error_for_invalid_sku():
     line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
@@ -44,8 +54,8 @@ def test_error_for_invalid_sku():
 def test_commits():
     line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
     batch = model.Batch("b1", "OMINOUS-MIRROR", 30, eta=None)
-    repo = FakeRepository()
-    session = FakeSession([batch])
+    repo = FakeRepository([batch])
+    session = FakeSession()
 
     services.allocate(line, repo, session)
     assert session.committed is True
