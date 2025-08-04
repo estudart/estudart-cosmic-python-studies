0ca1642a7bcd1263c05d2de2fcd387a75917c031
estudart <erico.studartdf@gmail.com>
2025-08-04 13:50:20 -0300

    fix: tests should run against the service

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -63,19 +63,6 @@ def test_commits():
     services.allocate(line, repo, session)
     assert session.committed is True
 
-# domain-layer test:
-def test_prefers_current_stock_batches_to_shipments():
-    in_stock_batch = model.Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
-    shipment_batch = model.Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
-    line = model.OrderLine("oref", "RETRO-CLOCK", 10)
-
-    model.allocate(line, [in_stock_batch, shipment_batch])
-
-    assert in_stock_batch.available_quantity == 90
-    assert shipment_batch.available_quantity == 100
-
-
-# service-layer test:
 def test_prefers_warehouse_batches_to_shipments():
     in_stock_batch = model.Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
     shipment_batch = model.Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
